3e9b0506db9463caf8bc4cb10f5e03ddcc2d5c96
Rohan Gada <rohangadaconsultant@localhost>
2017-10-18 16:29:55 +0530

    Multiple reducers

--- src/js/client.js
+++ src/js/client.js
@@ -1,12 +1,31 @@
-import {createStore} from 'redux'
+import {
+  createStore,
+  combineReducers
+} from 'redux'
 
-const reducer = (state,action) =>{
+const userReducer = (state = {}, action) => {
   switch (action.type) {
-    case 'INC':
-      state += action.payload
+    case 'CHANGE_NAME':
+      state = { ...state,
+        name: action.payload
+      };
       break;
-    case 'DEC':
-      state -= action.payload
+    case 'CHANGE_AGE':
+      state = { ...state,
+        age: action.payload
+      };
+      break;
+
+    default:
+      break;
+  }
+  return state;
+}
+
+const tweetsReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      state = [...state,action.payload]
       break;
   
     default:
@@ -14,29 +33,35 @@ const reducer = (state,action) =>{
   }
   return state;
 }
-const store = createStore(reducer,0);
 
-store.subscribe(()=>{
-  console.log('Store changed',store.getState())
+
+const reducers = combineReducers({
+  user: userReducer,
+  tweets: tweetsReducer
+})
+const store = createStore(reducers);
+
+store.subscribe(() => {
+  console.log('Store changed', store.getState())
 })
 
 store.dispatch({
-  type:'INC',
+  type: 'CHANGE_NAME',
+  payload: 'Gada'
+}) 
+store.dispatch({
+  type: 'CHANGE_AGE',
   payload: 1
 })
 store.dispatch({
-  type:'INC',
+  type: 'CHANGE_NAME',
+  payload: 'Rohan'
+})
+store.dispatch({
+  type: 'INC',
   payload: 1
 })
 store.dispatch({
-  type:'DEC',
-  payload: 1
-})
-store.dispatch({
-  type:'INC',
-  payload: 1
-})
-store.dispatch({
-  type:'INC',
+  type: 'INC',
   payload: 1
 })
